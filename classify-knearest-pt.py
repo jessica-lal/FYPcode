d466c72908c12ab79f22e1d5ef06da8cde7babda
jessica-lal <56547183+jessica-lal@users.noreply.github.com>
2021-07-03 02:34:27 +0100

    Add files via upload

--- classify-knearest-pt.py
+++ classify-knearest-pt.py
@@ -12,33 +12,34 @@ X = training_dataset[:, 0:-1]
 y = training_dataset[:, -1]
 
 # manually change the number of neighbours from 1 to 25, in jumps of 1
-neighbors = 25
-classifier = KNeighborsClassifier(n_neighbors=neighbors)
+for neighbors in range(1, 26):
+    classifier = KNeighborsClassifier(n_neighbors=neighbors)
 
-print("")
-print("The validation used in this experiment is:")
-k_fold = KFold(n_splits=10)
-print(k_fold)
-print("")
-print("The classifier used in this experiment is:")
-print(classifier)
-print("=======")
-print("Training performed on the training data:")
+    print("")
+    print("The validation used in this experiment is:")
+    k_fold = KFold(n_splits=10)
+    print(k_fold)
+    print("")
+    print("The classifier used in this experiment is:")
+    print(classifier)
+    print("=======")
+    print("Training performed on the training data:")
 
-score_collection = []
-fold_number = 0
-for train_index, test_index in k_fold.split(X):
-    print("Fold: " + str(fold_number))
-    X_train, X_test = X[train_index], X[test_index]
-    y_train, y_test = y[train_index], y[test_index]
+    score_collection = []
+    fold_number = 0
 
-    classifier.fit(X_train, y_train)
+    for train_index, test_index in k_fold.split(X):
+        print("Fold: " + str(fold_number))
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]
 
-    predictions = classifier.predict(X_test)
-    accuracy = accuracy_score(y_test, predictions)
-    score_collection.append(accuracy)
-    fold_number = fold_number + 1
-accuracy = np.mean(score_collection)
-standard_deviation = np.std(score_collection)
-print("Finished training via validation:")
-print("Accuracy: " + str(accuracy * 100) + ", Standard Deviation: " + str(standard_deviation * 100))
+        classifier.fit(X_train, y_train)
+
+        predictions = classifier.predict(X_test)
+        accuracy = accuracy_score(y_test, predictions)
+        score_collection.append(accuracy)
+        fold_number = fold_number + 1
+    accuracy = np.mean(score_collection)
+    standard_deviation = np.std(score_collection)
+    print("Finished training via validation:")
+    print("Accuracy: " + str(accuracy * 100) + ", Standard Deviation: " + str(standard_deviation * 100))
--- classify-randomforest-pt.py
+++ classify-randomforest-pt.py
@@ -12,33 +12,33 @@ X = training_dataset[:, 0:-1]
 y = training_dataset[:, -1]
 
 # manually change the number of trees from 10 to 200, in jumps of 10
-trees = 200
-classifier = RandomForestClassifier(n_estimators=trees)
+for trees in range(10, 201, 10):
+    classifier = RandomForestClassifier(n_estimators=trees)
 
-print("")
-print("The validation used in this experiment is:")
-k_fold = KFold(n_splits=10)
-print(k_fold)
-print("")
-print("The classifier used in this experiment is:")
-print(classifier)
-print("=======")
-print("Training performed on the training data:")
+    print("")
+    print("The validation used in this experiment is:")
+    k_fold = KFold(n_splits=10)
+    print(k_fold)
+    print("")
+    print("The classifier used in this experiment is:")
+    print(classifier)
+    print("=======")
+    print("Training performed on the training data:")
 
-score_collection = []
-fold_number = 0
-for train_index, test_index in k_fold.split(X):
-    print("Fold: " + str(fold_number))
-    X_train, X_test = X[train_index], X[test_index]
-    y_train, y_test = y[train_index], y[test_index]
+    score_collection = []
+    fold_number = 0
+    for train_index, test_index in k_fold.split(X):
+        print("Fold: " + str(fold_number))
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]
 
-    classifier.fit(X_train, y_train)
+        classifier.fit(X_train, y_train)
 
-    predictions = classifier.predict(X_test)
-    accuracy = accuracy_score(y_test, predictions)
-    score_collection.append(accuracy)
-    fold_number = fold_number + 1
-accuracy = np.mean(score_collection)
-standard_deviation = np.std(score_collection)
-print("Finished training via validation:")
-print("Accuracy: " + str(accuracy * 100) + ", Standard Deviation: " + str(standard_deviation * 100))
+        predictions = classifier.predict(X_test)
+        accuracy = accuracy_score(y_test, predictions)
+        score_collection.append(accuracy)
+        fold_number = fold_number + 1
+    accuracy = np.mean(score_collection)
+    standard_deviation = np.std(score_collection)
+    print("Finished training via validation:")
+    print("Accuracy: " + str(accuracy * 100) + ", Standard Deviation: " + str(standard_deviation * 100))
